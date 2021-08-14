e0cb52bc3e2d0c8043d003b739ff6118196aaf32
alcoimbra88 <al_coimbra@hotmail.com>
2021-08-14 03:42:08 -0300

    1 Commit - Gera CPF - NPM - WebPack - Node

---
+++ src/modules/ValidaCPF.js
@@ -0,0 +1,46 @@
+export default class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            value: cpfEnviado.replace(/\D+/g, ''),
+            writable: false,
+            configurable: false,
+            enumerable: false
+        });
+    };
+
+    static geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+        for (let numero of cpfSemDigitos) {
+            total += reverso * Number(numero);
+            reverso--;
+        };
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
+    };
+
+    isSequence() {
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    };
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1)
+        this.cpfGerado = cpfSemDigitos + digito1 + digito2;
+    };
+
+
+    valida() {
+        if (!this.cpfLimpo) return;
+        if (typeof this.cpfLimpo !== 'string') return;
+        if (this.cpfLimpo.length !== 11) return;
+        if (this.isSequence()) return false;
+        this.geraNovoCpf();
+
+        return this.cpfLimpo === this.cpfGerado ;
+    };
+};
+
+console.log('cheguei aqui');
